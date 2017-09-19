4121e6bb27253ea7d152e02e5dc780bc5022003b
Gianluca Ciarcelluti <gianluca@ciarcelluti.it>
2017-09-20 01:28:49 +0200

    Add: automate index.rst for readthedocs.io building on npm run

--- post-local-test.sh
+++ post-local-test.sh
@@ -4,3 +4,5 @@
 cp src/bootstrap.data-driven-components.js docs/bootstrap.data-driven-components.js
 ./node_modules/.bin/jsdox --output docs src
 cat docs/introduction.md docs/bootstrap.data-driven-components.md > README.md
+# build readthedocs.io documentation
+pandoc -s -r markdown -w rst  README.md -o docs/index.rst
